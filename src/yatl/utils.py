3894ebe60b558547bf46d8c8cb0fa50050a7bd24
khabib shahbanov <khabibtime@rambler.ru>
2026-03-05 19:16:22 +0300

    add search all filex with suffix .test.yaml

--- src/yatl/utils.py
+++ src/yatl/utils.py
@@ -1,7 +1,7 @@
 import os
 
 
-def search(current_path, item, files):
+def search(current_path, item: str, files: list):
     full_path = os.path.join(current_path, item)
     if os.path.isfile(full_path) and (
         item.endswith(".test.yaml") or item.endswith(".test.yml")
